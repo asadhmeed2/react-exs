2c1abe326e17a08bec563bc55b97c27da9ad782b
newasad <asadhmeed2@gmail.com>
2021-10-17 12:45:12 +0300

    ex2-7.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/ex2-1/ex2-1'
+import Hello from './components/ex2-2/ex2-2.1'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+     
         <Hello/>
         
-      </header>
+     
     </div>
   );
 }
