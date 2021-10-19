f4d3b84b6501c1f210d2bbf15c6b94ae6daf843f
newasad <asadhmeed2@gmail.com>
2021-10-20 00:30:37 +0300

    add categories

--- src/components/ex12.1/chuckNorris.jsx
+++ src/components/ex12.1/chuckNorris.jsx
@@ -6,24 +6,50 @@ import './chuckNorris.css'
 export default class ChuckNorris extends React.Component {
     constructor() {
     super();
-    this.state = { jokes: "" , img:""};
+    this.state = { jokes: "" , img:"",dataByCategory:[] ,category:"all", categories: []};
     }
     getJoke=async()=>{
          try {
            let data = await (
              await fetch("https://api.chucknorris.io/jokes/random")
            ).json();
-       console.log(data);
            this.setState({ jokes: data.value, img: data.icon_url });
          } catch (err) {
            console.log(err);
          }
     }
-
+    getJokeByCategory=async()=>{
+         try {
+           let data = await (
+             await fetch(
+               `https://api.chucknorris.io/jokes/random?category=${this.state.category}`
+             )
+           ).json();
+           this.setState({ jokes: data.value});
+         } catch (err) {
+           console.log(err);
+         }
+    }
+    getCategories =async()=>{
+         try {
+           let data = await (
+             await fetch(`https://api.chucknorris.io/jokes/categories`)
+           ).json();
+           this.setState({ categories: data });
+         } catch (err) {
+           console.log(err);
+         }
+    }
     componentDidMount= async ()=> {     
             this.getJoke();
+            this.getCategories();
+    }
+    selectHandler=(e)=>{
+    
+        this.setState({category:e.target.value});
     }
     render() {
+        let jokeHandler = this.getJoke;
         return (
           <div className="chuckNorris">
             <div
@@ -32,7 +58,14 @@ export default class ChuckNorris extends React.Component {
                 background: `url(${this.state.img}) no-repeat center center/cover`,
               }}
             ></div>
-            <Button getJoke={this.getJoke} />
+
+            <Button getJoke={this.state.category==="all"?this.getJoke:this.getJokeByCategory} />
+            <select className="select" onChange={this.selectHandler}>
+              <option value="all">All</option>
+              {this.state.categories.map((category) => {
+                return <option value={category}>{category}</option>;
+              })}
+            </select>
             <div className="jockText">{this.state.jokes}</div>
           </div>
         );
